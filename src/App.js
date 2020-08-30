c955a641766b1a79ae47ad178c0105ffe5310048
Kelli Jarrell <kelli.jarrell@ninelineapparel.com>
2020-08-30 18:31:45 -0400

    added login page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Homepage from  "./components/pages/Homepage";
 import Admin from  "./components/pages/Admin";
 import Messages from "./components/pages/Messages";
 import "./components/assets/all-pages-style.css";
+import Login from "./components/pages/Login";
 
 function App(){
     return(
@@ -14,6 +15,7 @@ function App(){
             <Route exact path = "/homepage" component={Homepage}/>
             <Route exact path = "/messages" component={Messages}/>
             <Route exact path = "/admin" component={Admin}/>
+            <Route exact path = "/login" component={Login}/>
 
 
         </div>
